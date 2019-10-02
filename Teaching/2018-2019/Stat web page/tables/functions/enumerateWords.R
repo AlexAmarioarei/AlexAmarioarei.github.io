e0d3d82596b00cc957fb809f0da0c69b92789b47
Alexandru Amarioarei <amarioareialexandru@gmail.com>
2019-10-02 07:50:41 +0300

    New Year 2019 - 2020 Courses

--- Teaching/2018-2019/Stat web page/tables/functions/enumerateWords.R
+++
@@ -1,9 +0,0 @@
-enumerateWords = function(words){
-  lwd = length(words)
-  
-  if (lwd == 1){
-    print(words)
-  }else{
-    paste0(paste0(words[1:(lwd-1)], collapse = ", "), " and ", words[lwd])
-  }
-}
