1842d3bb9b8f329f703501eeb5d3b9fa840788b3
Alexandru Amarioarei <amarioareialexandru@gmail.com>
2020-03-18 12:09:28 +0200

    update Reg and InstStat webpages

---
+++ Teaching/2019-2020/Reg web page/tables/functions/getOutputFormat.R
@@ -0,0 +1,10 @@
+getOutputFormat <- function() {
+  output <- rmarkdown:::parse_yaml_front_matter(
+    readLines(knitr::current_input())
+  )$output
+  if (is.list(output)){
+    return(names(output)[1])
+  } else {
+    return(output[1])
+  }
+}
